adf4bb040e4aa6fb93836b226b5cebc135cc0c4f
Sanchit Sharma <sanchit.sharma99@gmail.com>
2018-10-06 22:24:14 +0530

    uploading existing work

---
+++ Assets/Hacker.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    // Game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+    string password;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ShowMainMenu ();
+    }
+
+    void ShowMainMenu ()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu") // we can always go direct to main menu
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        bool isValidLevelNumber  = (input=="1"||input=="2");
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            StartGame();
+        }
+        
+        else if (input == "007") //easter egg
+        {
+            Terminal.WriteLine("Please select a level Mr Bond!");
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Please enter your password: ");
+    }
+
+    void CheckPassword(string input)
+    {
+        if (true)
+        {
+            Terminal.WriteLine("WELL DONE!");
+        }
+        else
+        {
+            Terminal.WriteLine("Sorry, wrong password!");
+        }
+    }
+}
